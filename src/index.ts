e0b3a2cc6a428e1c033e003b0e6a52958cb70289
rendurok <90926651+rendurok@users.noreply.github.com>
2024-02-16 15:52:10 +0200

    add parsing of generic lines

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { addNavbarButton } from './render/buttons';
 import { renderCurrentConversations } from './render/cnvTree';
+import { addMenu } from './render/menu';
 
 addNavbarButton('cnv-parse-button', 'CNV', () => renderCurrentConversations());
+addMenu();
